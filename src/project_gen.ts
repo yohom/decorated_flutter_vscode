129cc220a2cad8774466805612f6ba116fb10513
yohom <yohombao@qq.com>
2023-07-25 16:30:29 +0800

    refactor: 调整模板所在文件夹

--- src/project_gen.ts
+++ src/project_gen.ts
@@ -3,21 +3,16 @@ import { workspaceDir } from './_helper';
 
 
 export async function handleProjectGen() {
-  // 生成预定的组件
-  await _genComponent()
-}
-
-async function _genComponent() {
   const extensionPath = vscode.extensions
     .getExtension('decoratedflutter.decorated-flutter-vscode')
     ?.extensionUri;
 
-  const componentsTmpl = extensionPath?.path + '/media/tmpl/components';
+  const componentsTmpl = extensionPath?.path + '/media/tmpl/project/components';
   vscode.workspace.fs.copy(
     vscode.Uri.file(componentsTmpl),
     vscode.Uri.file(`${workspaceDir()}/components`),
   );
-  const modulesTmpl = extensionPath?.path + '/media/tmpl/modules';
+  const modulesTmpl = extensionPath?.path + '/media/tmpl/project/modules';
   vscode.workspace.fs.copy(
     vscode.Uri.file(modulesTmpl),
     vscode.Uri.file(`${workspaceDir()}/modules`),
